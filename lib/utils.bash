99ef009c7ba522cce4e0639282da7469def15474
Nathan Smyth <nathan.smyth@mongodb.com>
2024-03-07 07:00:48 +0000

    feat: Use M for latest-stable & list-all

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,10 +2,11 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for mongodb.
-GH_REPO="https://github.com/jarjee/asdf-mongodb"
 TOOL_NAME="mongodb"
 TOOL_TEST="mongodb --help"
+
+current_script_path=${BASH_SOURCE[0]}
+util_dir=$(dirname "$current_script_path")
 
 fail() {
 	echo -e "asdf-$TOOL_NAME: $*"
@@ -14,26 +15,17 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if mongodb is not hosted on GitHub releases.
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
-
 sort_versions() {
 	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
 		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
-list_github_tags() {
-	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+list_all_versions() {
+	"$util_dir/m" ls
 }
 
-list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if mongodb has other means of determining installable versions.
-	list_github_tags
+list_stable_version() {
+  "$util_dir/m" --stable
 }
 
 download_release() {
